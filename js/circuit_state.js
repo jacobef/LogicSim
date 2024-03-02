94ee7a2221e4214530e98dfb31bfc932dbeedd2c
jacobef <jacobef2@gmail.com>
2024-03-02 15:32:10 -0700

    add location, radius, label to visible things

--- js/circuit_state.js
+++ js/circuit_state.js
@@ -1,5 +1,19 @@
-class SimpleGate {
-    constructor(connectionsIn) {
+class VisibleThing {
+    constructor(x, y, radius, label) {
+        console.assert(0 <= x && x <= 1);
+        this.x = x;
+        console.assert(0 <= y && y <= 1);
+        this.y = y;
+        console.assert(0 <= radius && radius <= 1);
+        this.radius = radius;
+        console.assert(typeof label === "string");
+        this.label = label;
+    }
+}
+
+class SimpleGate extends VisibleThing {
+    constructor(connectionsIn, x, y, label) {
+        super(x, y, 0.05, label);
         console.assert(connectionsIn instanceof Array)
         for (const conn of connectionsIn) {
             console.assert(conn instanceof GateConnection);
@@ -8,7 +22,7 @@ class SimpleGate {
     }
 
     getState(visited= []) {
-        const bool_inputs = []
+        const bool_inputs = [];
         for (const conn of this.connectionsIn) {
             console.log(conn.gateOrInput);
             if (visited.includes(conn.gateOrInput) || this === conn.gateOrInput) {
@@ -27,8 +41,9 @@ class SimpleGate {
     }
 }
 
-class SimpleInput {
-    constructor(initialState) {
+class SimpleInput extends VisibleThing {
+    constructor(initialState, x, y, label) {
+        super(x, y, 0.025, label);
         console.assert(typeof initialState === "boolean");
         this.state = initialState;
     }
@@ -38,9 +53,18 @@ class SimpleInput {
     }
 }
 
-class SimpleOutput {
-    constructor(initialState) {
-        this.state = initialState;
+class SimpleOutput extends VisibleThing {
+    constructor(connection, x, y, label) {
+        super(x, y, 0.025, label);
+        console.assert(connection instanceof GateConnection || connection === null);
+        this.connection = connection;
+    }
+    getState() {
+        if (this.connection === null) {
+            return [false];
+        } else {
+            return this.connection.gateOrInput.getState()[this.connection.index];
+        }
     }
 }
 
@@ -91,16 +115,19 @@ class NorGate extends SimpleGate {
 }
 
 function exampleUsage() {
-    const in1 = new SimpleInput(false);
-    const in2 = new SimpleInput(false);
+    const in1 = new SimpleInput(false, 0.05, 0.05, "INPUT 1");
+    const in2 = new SimpleInput(false, 0.05, 0.1, "INPUT 2");
     const nor1 = new NorGate(
-        [new GateConnection(in1, 0), new GateConnection(in2, 0)]
+        [new GateConnection(in1, 0), new GateConnection(in2, 0)],
+        0.2, 0.1, "NOR 1"
     );
     const nor2 = new NorGate(
-        [new GateConnection(in1, 0), new GateConnection(in2, 0)]
+        [new GateConnection(in1, 0), new GateConnection(in2, 0)],
+        0.2, 0.2, "NOR 2"
     );
     nor1.connectionsIn[0] = new GateConnection(nor2, 0);
     nor2.connectionsIn[1] = new GateConnection(nor1, 0);
+    const out = new SimpleOutput(new GateConnection(nor2, 0), 0.6, 0.5, "OUT");
 
-    console.log(nor1.getState());
+    console.log(out.getState());
 }
