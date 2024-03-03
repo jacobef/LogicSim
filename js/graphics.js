0831b0e1f980869aee5ff3197933260f7ebf4594
Duncan Britt <dbru997@gmail.com>
2024-03-02 21:17:01 -0700

    stuff

--- js/graphics.js
+++ js/graphics.js
@@ -50,6 +50,34 @@ hands.hour.linewidth = 10;
 hands.hour.cap = 'round';
 two.add(hands.hour);
 
+class Knob {
+    constructor(parent_circle, knob_num) {
+        this.parent_circle = parent_circle;
+        this.radius = Math.floor(this.parent_circle.radius/3);
+        this.knob_num = knob_num;
+        this._shape = new Two.Circle(0,0, parent_circle.radius/3, 32);
+        this._circle = new Two.Path(this._shape.vertices, true, true);
+        const [ x, y ] = this.position();
+        this._circle.position.set(x,y);
+        this._circle.noStroke().fill = 'black';
+        two.add(this._circle);
+    }    
+
+    position() {
+        switch (this.knob_num) {
+        case 1:
+            return [this.parent_circle.center[0] - this.parent_circle.radius * Math.cos(Math.PI/6), this.parent_circle.center[1] + this.parent_circle.radius * Math.sin(Math.PI/6)];
+            break;
+        case 2:
+            return [this.parent_circle.center[0] - this.parent_circle.radius * Math.cos(Math.PI/6), this.parent_circle.center[1] - this.parent_circle.radius * Math.sin(Math.PI/6)];
+            break;
+        case 3:
+            return [this.parent_circle.center[0] + this.parent_circle.radius, this.parent_circle.center[1]];
+            break;
+        }
+    }
+}
+
 class Circle {
     constructor({ center, radius, label, logic }) {
         this.center = center;
@@ -66,30 +94,34 @@ class Circle {
         this._circle = new Two.Path(this._shape.vertices, true, true);
         this._circle.position.set(center[0],center[1]);
         this._circle.noStroke().fill = 'white';
-        two.add(this._circle);        
+        two.add(this._circle);
+        this.input_knobs = [];        
         
         if (!(logic instanceof circuit_state.SimpleInput)) {
-            const [ nob1x, nob1y ] = this.nob1_position();
-            this.nob1_shape = new Two.Circle(nob1x, nob1y, radius/3, 32);
-            this.nob1_circle = new Two.Path(this.nob1_shape.vertices, true, true);
-            this.nob1_circle.position.set(nob1x, nob1y);
-            this.nob1_circle.noStroke().fill = 'black';
-            two.add(this.nob1_circle);
+            this.input_knobs[0] = new Knob(this, 1);
+            this.input_knobs[1] = new Knob(this, 2);
+            // const [ nob1x, nob1y ] = this.nob1_position();
+            // this.nob1_shape = new Two.Circle(nob1x, nob1y, radius/3, 32);
+            // this.nob1_circle = new Two.Path(this.nob1_shape.vertices, true, true);
+            // this.nob1_circle.position.set(nob1x, nob1y);
+            // this.nob1_circle.noStroke().fill = 'black';
+            // two.add(this.nob1_circle);
 
-            const [ nob2x, nob2y ] = this.nob2_position();
-            this.nob2_shape = new Two.Circle(nob2x, nob2y, radius/3, 32);
-            this.nob2_circle = new Two.Path(this.nob2_shape.vertices, true, true);
-            this.nob2_circle.position.set(nob2x, nob2y);
-            this.nob2_circle.noStroke().fill = 'black';
-            two.add(this.nob2_circle);
+            // const [ nob2x, nob2y ] = this.nob2_position();
+            // this.nob2_shape = new Two.Circle(nob2x, nob2y, radius/3, 32);
+            // this.nob2_circle = new Two.Path(this.nob2_shape.vertices, true, true);
+            // this.nob2_circle.position.set(nob2x, nob2y);
+            // this.nob2_circle.noStroke().fill = 'black';
+            // two.add(this.nob2_circle);
         }        
 
-        const [ nob3x, nob3y ] = this.nob2_position();
-        this.nob3_shape = new Two.Circle(0, 0, radius/3, 32);
-        this.nob3_circle = new Two.Path(this.nob3_shape.vertices, true, true);
-        this.nob3_circle.position.set(nob3x, nob3y);
-        this.nob3_circle.noStroke().fill = 'black';
-        two.add(this.nob3_circle);
+        this.output_knob = new Knob(this, 3);
+        // const [ nob3x, nob3y ] = this.nob2_position();
+        // this.nob3_shape = new Two.Circle(0, 0, radius/3, 32);
+        // this.nob3_circle = new Two.Path(this.nob3_shape.vertices, true, true);
+        // this.nob3_circle.position.set(nob3x, nob3y);
+        // this.nob3_circle.noStroke().fill = 'black';
+        // two.add(this.nob3_circle);
     }
 
     nob1_position() {
@@ -177,31 +209,31 @@ canvas.addEventListener('mousedown', (e) => {
     for (const circ of circles) {
         let is_within = within([mouse.x, mouse.y], circ.center, circ.radius);        
         if (is_within) {            
-            circ.dragging = true;
+            circ.dragging = true;            
             break;
         }
-
-        const nob1_position = circ.nob1_position();
-        const is_within_nob1 = within([mouse.x, mouse.y], nob1_position, circ.radius/3);
-        console.log(nob1_position);
         
-        if (is_within_nob1) {
-            console.log('here');
-            circ.nob1_line = new Two.Line(nob1_position[0], nob1_position[1], mouse.x, mouse.y);
-            circ.nob1_line.noFill();
-            circ.nob1_line.stroke = 'blue';
-            circ.nob1_line.linewidth = 10;
-            circ.nob1_line.cap = 'round';
-            two.add(circ.nob1_line);
-        }
-
-        const nob2_position = circ.nob2_position();
-        const is_within_nob2 = within([mouse.x, mouse.y], nob2_position, circ.radius/3);
-
-        const nob3_position = circ.nob3_position();
-        const is_within_nob3 = within([mouse.x, mouse.y], nob3_position, circ.radius/3);
+        circ.input_knobs.concat([circ.output_knob]).forEach(knob => {            
+            const is_within = within([mouse.x, mouse.y], knob.position(), knob.radius);
+            if (is_within) {
+                const [ x, y ] = knob.position();
+                knob.line = new Two.Line(x, y, mouse.x, mouse.y);
+            }
+        });                
     }    
 });
+
+// const hands = {
+//     hour: new Two.Line(100, 100, 200, 200),
+//     minute: new Two.Line(0, 0, 0, - 50 * 0.8),
+//     second: new Two.Line(0, 0, 0, - 50 * 0.9)
+// };
+
+// hands.hour.noFill();
+// hands.hour.stroke = 'blue';
+// hands.hour.linewidth = 10;
+// hands.hour.cap = 'round';
+// two.add(hands.hour);
 
 canvas.addEventListener('mousemove', (e) => {
     mouse.x = e.clientX;
@@ -212,15 +244,21 @@ canvas.addEventListener('mousemove', (e) => {
             circ.center = [mouse.x, mouse.y];            
         }
 
-        if (circ.nob1_line) {
-            two.remove(circ.nob1_line);
-            const nob1_position = circ.nob1_position();
-            circ.nob1_line.noFill();
-            circ.nob1_line.stroke = 'blue';
-            circ.nob1_line.linewidth = 10;
-            circ.nob1_line.cap = 'round';
-            two.add(circ.nob1_line);
-        }
+        circ.input_knobs.concat([circ.output_knob]).forEach(knob => {
+            
+            if (knob.line) {
+                two.remove(knob.line);                    
+
+                const [ x, y ] = knob.position(); 
+                
+                knob.line = new Two.Line(x, y, mouse.x, mouse.y);
+                knob.line.noFill();
+                knob.line.stroke = 'blue';
+                knob.linewidth = 20;
+                knob.line.cap = 'round';
+                two.add(knob.line);
+            }
+        });                
     });    
 });
 
@@ -230,7 +268,28 @@ canvas.addEventListener('mouseup', (e) => {
     mouse.x = e.clientX;
     mouse.y = e.clientY;
 
-    circles.forEach(circ => {        
+    circles.forEach(circ => {
+        circ.input_knobs.concat([circ.output_knob]).forEach((knob,i) => {            
+            if (knob.line) {
+                // get other knobs
+                const other_knobs = circ.input_knobs.concat([circ.output_knob]).filter((knob, j) => {
+                    return j != i;
+                });
+
+                if (other_knobs.some(other_knob => {                    
+                    let is_within = within([mouse.x, mouse.y], other_knob.position(), knob.radius);
+                    if (is_within) {
+                        if (other_knob.parent_circle.logic instanceof circuit_state.SimpleOutput) {
+                            
+                        }
+                    }
+                })) 
+                
+                two.remove(knob.line);
+                knob.line = null;                
+            }
+        });
+        
         // console.log(circ);
         // console.log(mouse.x, mouse.y, circ.center, circ.radius);
         let is_within = within([mouse.x, mouse.y], [circ.center[0], circ.center[1]], circ.radius);
@@ -256,14 +315,16 @@ two.bind('update', function() {
     circles.forEach(circ => {        
         circ._circle.position.set(circ.center[0], circ.center[1]);
         circ._circle.noStroke().fill = circ.logic.getState()[0] ? 'red' : 'white';
-
         circ.label.translation.set(circ.center[0], circ.center[1] - circ.radius - 2);
 
-        if (!(circ.logic instanceof circuit_state.SimpleInput)) {            
-            circ.nob1_circle.position.set(circ.center[0] - circ.radius * Math.cos(Math.PI/6), circ.center[1] + circ.radius * Math.sin(Math.PI/6));
-            circ.nob2_circle.position.set(circ.center[0] - circ.radius * Math.cos(Math.PI/6), circ.center[1] - circ.radius * Math.sin(Math.PI/6));
-        }        
-        circ.nob3_circle.position.set(circ.center[0] + circ.radius, circ.center[1]);
+        if (!(circ.logic instanceof circuit_state.SimpleInput)) {
+            circ.input_knobs.forEach(knob => {
+                const [ x, y ] = knob.position();
+                knob._circle.position.set(x,y);
+            });            
+        }
+        const [ x, y ] = circ.output_knob.position();
+        circ.output_knob._circle.position.set(x,y);        
     });
 });
 
